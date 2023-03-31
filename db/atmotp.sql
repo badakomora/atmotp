749c329e934010400e16541383f1cfbb524a31aa
badaa <badaandrew@outlook.com>
2023-03-31 11:29:51 +0300

    db update

--- db/atmotp.sql
+++ db/atmotp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 23, 2023 at 07:37 AM
+-- Generation Time: Mar 31, 2023 at 10:27 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -24,6 +24,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `fullname`, `email`, `password`) VALUES
+(1, 'Administrator', 'admin@smartatm.com', '$2y$10$H6HjaYgTBCcj71Gkq6apF.wGraJPFVF.LHquJ1n4RbWA2N7IRhdEq');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `bal`
 --
 
@@ -38,7 +58,28 @@ CREATE TABLE `bal` (
 --
 
 INSERT INTO `bal` (`id`, `user_id`, `amount`) VALUES
-(1, 16, 5000);
+(8, 26, 900);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `deposits`
+--
+
+CREATE TABLE `deposits` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `deposit_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `transaction` varchar(50) NOT NULL DEFAULT 'Deposit'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `deposits`
+--
+
+INSERT INTO `deposits` (`id`, `user_id`, `amount`, `deposit_time`, `transaction`) VALUES
+(27, 26, 1000, '2023-03-31 06:56:06', 'Deposit');
 
 -- --------------------------------------------------------
 
@@ -50,8 +91,10 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `fullname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
+  `phone` varchar(50) NOT NULL,
   `accountno` int(9) NOT NULL,
   `cardno` int(16) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT 1,
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -59,18 +102,51 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `fullname`, `email`, `accountno`, `cardno`, `password`) VALUES
-(16, 'Bada komora', 'badakomora06@gmail.com', 9682826, 586246404, '$2y$10$O5xWqvf1vimmTWYjV0iobOeQYWK/g/pLFh/8DWjsK//kEoLtEMNP6'),
-(17, 'Ndondu Grace', 'ndondugrace88@gmail.com', 1406890, 146527364, '$2y$10$t4p4HDaLGM.TfpQiD7EreOA5NYWwdd6SaphToIgVF4BgdbAG2K4xi');
+INSERT INTO `users` (`id`, `fullname`, `email`, `phone`, `accountno`, `cardno`, `status`, `password`) VALUES
+(26, 'Andrew Bada', 'badakomora06@gmail.com', '0768438291', 8935665, 502246880, 0, '$2y$10$TsDsE6kUfLhjzc4xNYzaUe7JJEQm5Ocj.BRVs/MG53yM3j2ZLccIq'),
+(27, 'bada komora', 'badakomora42@gmail.com', '0759621394', 9924037, 538223135, 0, '$2y$10$jfoitNFVVR1JBENEcptjcOBxbGzZIXzhUpEX5Neje4/medyaGW6OO');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `withdrawals`
+--
+
+CREATE TABLE `withdrawals` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `withdrawal_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `transaction` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `withdrawals`
+--
+
+INSERT INTO `withdrawals` (`id`, `user_id`, `amount`, `withdrawal_time`, `transaction`) VALUES
+(5, 26, 100, '2023-03-31 08:22:09', 'Withdrawal');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `bal`
 --
 ALTER TABLE `bal`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `deposits`
+--
+ALTER TABLE `deposits`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -80,20 +156,44 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `withdrawals`
+--
+ALTER TABLE `withdrawals`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `bal`
 --
 ALTER TABLE `bal`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `deposits`
+--
+ALTER TABLE `deposits`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `withdrawals`
+--
+ALTER TABLE `withdrawals`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
